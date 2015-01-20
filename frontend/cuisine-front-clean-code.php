3c7d5a0718e2801b22ea6ffcdbec9b4b784589e3
Luc Princen <luc@chefduweb.nl>
2015-01-20 06:55:29 +0100

    Removed a redundant function

--- frontend/cuisine-front-clean-code.php
+++ frontend/cuisine-front-clean-code.php
@@ -1,20 +1,5 @@
 <?php
 
-	/**
-	*	Annoying little hack to maintain clean code when we use WordPress' Recent comments widget,
-	*	The css is in the Chef du Web theme's reset file
-	*
- 	* @access public
-	* @return void
-	*/
-
-	add_action( 'widgets_init', 'chef_remove_recent_comments_style' );
-	
-	function chef_remove_recent_comments_style() {
-		global $wp_widget_factory;
-		remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'  ) );
-	}
-	
 
 	/**
 	*	Generate a fluid bootstrap row based on the current loop:
